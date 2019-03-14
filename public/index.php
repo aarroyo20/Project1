81f37eeb360b5d83c6f100e41c942c1dff2919ec
Angela Arroyo <ara55@njit.edu>
2019-03-14 01:03:40 -0400

    edited csv file with names

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<?php
+
+$row = 1;
+if (($handle = fopen("names.csv", "r")) !== FALSE) {
+
+    echo '<table class="table table-striped">';
+
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<thead><tr>';
+        }else{
+            echo '<tr>';
+        }
+
+        for ($c=0; $c < $num; $c++) {
+            if(empty($data[$c])) {
+                $value = "&nbsp;";
+            }else{
+                $value = $data[$c];
+            }
+            if ($row == 2) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
+            }
+        }
+
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
+        }
+        $row++;
+    }
+
+    echo '</tbody></table>';
+    fclose($handle);
+}
+?>
